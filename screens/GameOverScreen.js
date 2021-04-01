ff9ee31e982f913fb2e0aaf6276c042cd54204a4
Dancan Chibole <dancankingstar@gmail.com>
2021-03-31 19:02:41 -0700

    text component

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,23 +1,24 @@
 import React from "react";
-import { StyleSheet, View, Button, Image } from "react-native";
+import { StyleSheet, View, Button, Image, Text } from "react-native";
 import BodyText from "../components/BodyText";
 import TitleText from "../components/TitleText";
+import Colors from "../constants/colors";
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
       <TitleText>The Game is Over!</TitleText>
       <View style={styles.imageContainer}>
-        <Image
-          style={styles.image}
-          source={{
-            uri:
-              "https://images.pexels.com/photos/1770809/pexels-photo-1770809.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-          }}
-        />
+        <Image style={styles.image} source={require("../assets/success.png")} />
       </View>
-      <BodyText>Number of rounds : {props.roundsNumber}</BodyText>
-      <BodyText>Number was : {props.userNumber}</BodyText>
+      <View style={styles.resultContainer}>
+        <BodyText style={styles.resultText}>
+          Your phone needed
+          <Text style={styles.highlight}> {props.roundsNumber}</Text> rounds to
+          guess the number
+          <Text style={styles.highlight}> {props.userNumber}</Text>
+        </BodyText>
+      </View>
       <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
   );
@@ -43,5 +44,17 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: "100%",
+  },
+  highlight: {
+    color: Colors.primary,
+    fontFamily: "open-sans-bold",
+  },
+  resultContainer: {
+    marginHorizontal: 40,
+    marginVertical: 15,
+  },
+  resultText: {
+    textAlign: "center",
+    fontSize: 20,
   },
 });
